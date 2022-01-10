89ee1b86dfa34a0540770c7dbfab3764705488bd
SAAluthwela <supunakalanka.git@gmail.com>
2022-01-10 19:23:41 +0530

    topBar done

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+import Sidebar from "./components/sidebar/Sidebar";
+import Topbar from "./components/topbar/Topbar";
+import "./app.css";
+
 function App() {
-  return <div className="App">Hello</div>;
+  return (
+    <div className="App">
+      <Topbar />
+      <div className="container">
+        <Sidebar />
+        <div className="others">others</div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
