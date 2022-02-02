35e16fcbb5de713390fe257d0adc554204fedca1
SAAluthwela <supunakalanka.git@gmail.com>
2022-02-03 00:02:57 +0530

    react-router-dom setup

--- src/App.js
+++ src/App.js
@@ -2,16 +2,21 @@ import Sidebar from "./components/sidebar/Sidebar";
 import Topbar from "./components/topbar/Topbar";
 import Home from "./pages/Home/Home";
 import "./app.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import UserList from "./pages/UserList/UserList";
 
 function App() {
   return (
-    <div className="App">
+    <Router>
       <Topbar />
       <div className="container">
         <Sidebar />
-        <Home />
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route path="/users" element={<UserList />} />
+        </Routes>
       </div>
-    </div>
+    </Router>
   );
 }
 
