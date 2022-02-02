78403d4565a23e9d16f67061353235935bed6800
SAAluthwela <supunakalanka.git@gmail.com>
2022-02-02 21:42:12 +0530

    finish home page

--- src/components/chart/Chart.jsx
+++ src/components/chart/Chart.jsx
@@ -10,14 +10,14 @@ import {
 } from "recharts";
 import "./Chart.css";
 
-export default function Chart({ title, data, datakey, grid }) {
+export default function Chart({ title, data, datakey, grid, stroke }) {
   return (
     <div className="chart">
       <h3 className="chartTitle">{title}</h3>
       <ResponsiveContainer width="100%" aspect={4 / 1}>
         <LineChart data={data}>
           <XAxis dataKey="name" stroke="#5550bd" />
-          <Line type="monotone" dataKey={datakey} stroke="#5550bd" />
+          <Line type="monotone" dataKey={datakey} stroke={stroke} />
           <Tooltip />
           {grid && <CartesianGrid stroke="#e0dfef" strokeDasharray="5 5" />}
           <Legend />
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Chart from "../../components/chart/Chart";
 import FeaturedInfo from "../../components/featuredInfo/featuredInfo";
 import { userData } from "../../dummyData";
+import { userData2 } from "../../dummyData2";
 import WidgetLg from "../../components/widgetLg/WidgetLg";
 import WidgetSm from "../../components/widgetSm/WidgetSm";
 
@@ -14,14 +15,22 @@ export default function Home() {
       <FeaturedInfo />
       <Chart
         data={userData}
-        title="Search Analytics"
+        title="User Search Analytics"
         datakey="search_hits"
         grid
+        stroke="#5550bd"
       />
       <div className="homeWidgets">
         <WidgetSm />
         <WidgetLg />
       </div>
+      <Chart
+        data={userData2}
+        title="Overall Kibana Analytics"
+        datakey="search_hits"
+        grid
+        stroke="#82ca9d"
+      />
     </div>
   );
 }
