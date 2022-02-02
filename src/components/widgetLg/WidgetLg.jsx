34e68986162a4b8f3ee848e62bea63a78b78ff1c
SAAluthwela <supunakalanka.git@gmail.com>
2022-02-02 17:56:38 +0530

    user list complete

--- src/components/widgetLg/WidgetLg.jsx
+++ src/components/widgetLg/WidgetLg.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 import "./widgetLg.css";
+import image from "../../img/profile.jpg";
 
-export default function widgetSm() {
+export default function widgetLg() {
+  const Button = ({ type }) => {
+    return <button className={"widgetLgButton " + type}>{type}</button>;
+  };
+
   return (
     <div className="widgetLg">
       <h3 className="widgetLgTitle"> Latest transactions</h3>
-      <table className="widgetLgtable">
+      <table className="widgetLgTable">
         <tr className="widgetLgTr">
           <th className="widgetLgTh"> Customer</th>
           <th className="widgetLgTh"> Date</th>
@@ -14,28 +19,68 @@ export default function widgetSm() {
         </tr>
 
         <tr className="widgetLgTr">
-          <td className="widgetLgUser"> Kelly Hsu</td>
+          <td className="widgetLgUser">
+            <img src={image} alt="" className="widgetLgImg" />
+            <span className="widgetLgName">Kelly Hsu</span>
+          </td>
           <td className="widgetLgDate"> 2021 - 05 - 10</td>
           <td className="widgetLgAmount"> $ 895</td>
-          <td className="widgetLgStatus"> Paid </td>
+          <td className="widgetLgStatus">
+            {" "}
+            <Button type="Approved" />{" "}
+          </td>
         </tr>
         <tr className="widgetLgTr">
-          <td className="widgetLgUser"> Lakmal Perera</td>
+          <td className="widgetLgUser">
+            {" "}
+            <img src={image} alt="" className="widgetLgImg" />
+            <span className="widgetLgName">Kelly Hsu</span>
+          </td>
           <td className="widgetLgDate"> 2022 - 01 - 10</td>
           <td className="widgetLgAmount"> $ 500</td>
-          <td className="widgetLgStatus"> Paid </td>
+          <td className="widgetLgStatus">
+            {" "}
+            <Button type="Pending" />{" "}
+          </td>
         </tr>
         <tr className="widgetLgTr">
-          <td className="widgetLgUser"> Chistena Mary</td>
+          <td className="widgetLgUser">
+            {" "}
+            <img src={image} alt="" className="widgetLgImg" />
+            <span className="widgetLgName">Kelly Hsu</span>
+          </td>
           <td className="widgetLgDate"> 2021 - 10 - 20</td>
           <td className="widgetLgAmount"> $ 950</td>
-          <td className="widgetLgStatus"> Pending </td>
+          <td className="widgetLgStatus">
+            {" "}
+            <Button type="Declined" />{" "}
+          </td>
         </tr>
         <tr className="widgetLgTr">
-          <td className="widgetLgUser"> Kelly Hsu</td>
+          <td className="widgetLgUser">
+            {" "}
+            <img src={image} alt="" className="widgetLgImg" />
+            <span className="widgetLgName">Kelly Hsu</span>
+          </td>
           <td className="widgetLgDate"> 2021 - 01 - 31</td>
           <td className="widgetLgAmount"> $ 100</td>
-          <td className="widgetLgStatus"> Paid </td>
+          <td className="widgetLgStatus">
+            {" "}
+            <Button type="Approved" />{" "}
+          </td>
+        </tr>
+        <tr className="widgetLgTr">
+          <td className="widgetLgUser">
+            {" "}
+            <img src={image} alt="" className="widgetLgImg" />
+            <span className="widgetLgName">Kelly Hsu</span>
+          </td>
+          <td className="widgetLgDate"> 2021 - 02 - 25</td>
+          <td className="widgetLgAmount"> $ 823</td>
+          <td className="widgetLgStatus">
+            {" "}
+            <Button type="Approved" />{" "}
+          </td>
         </tr>
       </table>
     </div>
